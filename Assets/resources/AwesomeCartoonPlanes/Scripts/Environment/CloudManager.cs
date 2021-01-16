9e21fc9f8a289db8ec2afe0fe5e1e90af113a759
KimSangWoo1 <59047886+KimSangWoo1@users.noreply.github.com>
2021-01-17 02:44:09 +0900

    Initial commit

---
+++ Assets/resources/AwesomeCartoonPlanes/Scripts/Environment/CloudManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+
+    private float cloudSpeed = 2.0f;
+
+    void Start()
+    {
+        
+    }
+
+  
+
+    void Update()
+    {
+        transform.Translate(transform.forward * cloudSpeed * Time.deltaTime, Space.Self);
+       
+    }
+}
